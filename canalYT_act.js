530e64d5c757e84e75cc259f5b0a5df0aca81a55
FCase <101079761+FCase@users.noreply.github.com>
2024-11-11 21:41:52 -0300

    v4yt

--- canalYT_act.js
+++ canalYT_act.js
@@ -1,34 +1,45 @@
 const apiKey = 'AIzaSyCPZAo26KrCpEHADcw-e1JCcGxZuPL7icI';
-const channelId = 'UCYhtVZwxFRNbppFt4GYRK6A';
-const maxResults = 4; 
+const playlistIds = ['PLFz2S11K1YEJN2MCBbmebs5BCmOoWyhBi&si=5gz-wXxqCO0JyEoK', 'PLFz2S11K1YEL93NPlpsP7KJHxQxoxTl6s&si=4apdYDEewoTQNiSI']; // Agrega más playlist IDs si lo necesitas
+const maxResults = 4;
+const videosContainer = document.getElementById('videos');
 
 const cachedVideos = JSON.parse(localStorage.getItem('youtubeVideosSaga'));
 const lastFetchTime = localStorage.getItem('lastFetchTime');
 const currentTime = new Date().getTime();
 
 if (cachedVideos && lastFetchTime && (currentTime - lastFetchTime < 12 * 60 * 60 * 1000)) {
-  // Si hay datos en caché y no ha pasado un día, usa los datos en caché
   displayVideos(cachedVideos);
 } else {
-  // Si no hay datos en caché o ha pasado un día, realiza una solicitud
-  fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}`)
-    .then(response => response.json())
-    .then(data => {
-      // Almacena los datos y el tiempo de solicitud en el almacenamiento local
-      localStorage.setItem('youtubeVideosSaga', JSON.stringify(data.items));
+  let allVideos = [];
+  Promise.all(
+    playlistIds.map(playlistId =>
+      fetch(`https://www.googleapis.com/youtube/v3/playlistItems?key=${apiKey}&playlistId=${playlistId}&part=snippet&maxResults=${maxResults}`)
+        .then(response => response.json())
+        .then(data => {
+          allVideos = allVideos.concat(data.items);
+        })
+    )
+  )
+    .then(() => {
+      // Ordenar videos por fecha de publicación descendente para mostrar los más recientes primero
+      allVideos.sort((a, b) => new Date(b.snippet.publishedAt) - new Date(a.snippet.publishedAt));
+
+      // Almacenar los datos en caché y mostrar los videos ordenados
+      localStorage.setItem('youtubeVideosSaga', JSON.stringify(allVideos));
       localStorage.setItem('lastFetchTime', currentTime);
-      displayVideos(data.items);
+      displayVideos(allVideos);
     })
-    .catch(error => console.error('Error fetching data:', error));
+    .catch(error => console.error('Error al obtener datos de la API:', error));
 }
 
 function displayVideos(videos) {
-  let videosContainer = document.getElementById('videos');
   videosContainer.innerHTML = '';
-  videos.forEach(item => {
-    let video = document.createElement('div');
-    video.innerHTML = `<iframe width=80% height="315" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>`;
-    videosContainer.appendChild(video);
+  videos.slice(0, maxResults).forEach(item => { // Mostrar solo los primeros `maxResults` videos después de ordenar
+    const videoContainer = document.createElement('div');
+    videoContainer.classList.add('video-container', 'col-md-4');
+    videoContainer.innerHTML = `
+      <iframe width="100%" height="315" src="https://www.youtube.com/embed/${item.snippet.resourceId.videoId}" frameborder="0" allowfullscreen></iframe>
+    `;
+    videosContainer.appendChild(videoContainer);
   });
 }
-
